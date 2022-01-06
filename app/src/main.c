2be1f13ba23c60bb58b2c0928beb3241a1244608
matejkrenek <mate23.krenek@gmail.com>
2022-01-06 23:20:26 +0100

    initial commit to stm8 playground repo

--- app/src/main.c
+++ app/src/main.c
@@ -3,14 +3,10 @@
 
 void main(void)
 {
-    CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1); // FREQ MCU 16MHz
-    GPIO_Init(GPIOC, GPIO_PIN_5, GPIO_MODE_OUT_PP_LOW_SLOW);
+    GPIO_Init(GPIOC, GPIO_PIN_5, GPIO_MODE_OUT_PP_HIGH_SLOW);
 
-    while (1)
-    {
-        GPIO_WriteReverse(GPIOC, GPIO_PIN_5);
-        for (uint32_t i = 0; i < 100000; i++)
-            ;
+    while(1) {
+        GPIO_WriteLow(GPIOC, GPIO_PIN_5);
     }
 }
 
